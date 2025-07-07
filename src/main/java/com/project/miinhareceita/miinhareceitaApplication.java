06fb1fac0156e54178bf205b0d74b3d2ae9c828e
Gustavo <kkowakskigustavo@gmail.com>
2025-07-07 15:41:47 -0300

    chore: Estrututura inicial do projeto criada

---
+++ src/main/java/com/project/miinhareceita/miinhareceitaApplication.java
@@ -0,0 +1,13 @@
+package com.project.miinhareceita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class miinhareceitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(miinhareceitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/miinhareceita/MinhaReceitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.miinhareceita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class miinhareceitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
