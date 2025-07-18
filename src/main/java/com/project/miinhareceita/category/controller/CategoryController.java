3ab633d8ed633fa4f931dd0ca05b7c56c8535da9
Gustavo <kkowakskigustavo@gmail.com>
2025-07-18 15:20:20 -0300

    doc: MediaType CategoryController

--- src/main/java/com/project/miinhareceita/category/controller/CategoryController.java
+++ src/main/java/com/project/miinhareceita/category/controller/CategoryController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +29,7 @@ import java.util.List;
                     @ApiResponse(description = "Ok", responseCode = "200"),
             }
     )
-        @GetMapping
+        @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
         public ResponseEntity<List<CategoryMinDTO>> findAll() {
             List<CategoryMinDTO> dto = service.findAll();
             return ResponseEntity.ok(dto);
--- src/main/java/com/project/miinhareceita/favorite/controller/FavoriteController.java
+++ src/main/java/com/project/miinhareceita/favorite/controller/FavoriteController.java
@@ -3,6 +3,7 @@ package com.project.miinhareceita.favorite.controller;
 import com.project.miinhareceita.favorite.dto.FavoriteDTO;
 import com.project.miinhareceita.favorite.dto.FavoriteInsertDTO;
 import com.project.miinhareceita.favorite.service.FavoriteService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Favorite", description = "Controller for favorites")
 @RestController
 @RequestMapping(value = "/favorites")
 public class FavoriteController {
