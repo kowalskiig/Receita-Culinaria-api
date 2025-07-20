804588c43ce5f055ab8ac9c9ea5eca2771e97c87
nawszin <kkowalskigustavo@gmail.com>
2025-07-20 16:59:14 -0300

    feat: testes unitários AuthService, com classe Util melhorando testes(100% cobertura)

--- src/main/java/com/project/miinhareceita/user/validation/UserInsertValidator.java
+++ src/main/java/com/project/miinhareceita/user/validation/UserInsertValidator.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class UserInsertValidator implements ConstraintValidator<UserInsertValid, UserInsertDTO> {
 	
@@ -25,7 +26,7 @@ public class UserInsertValidator implements ConstraintValidator<UserInsertValid,
 		
 		List<FieldMessage> list = new ArrayList<>();
 		
-		User user = repository.findByEmail(dto.getEmail());
+		Optional<User> user = repository.findByEmail(dto.getEmail());
 		if (user != null) {
 			list.add(new FieldMessage("email", "Email já existe"));
 		}
---
+++ src/test/java/com/project/miinhareceita/services/AuthServiceTest.java
@@ -0,0 +1,65 @@
+package com.project.miinhareceita.services;
+
+import com.project.miinhareceita.auth.service.AuthService;
+import com.project.miinhareceita.auth.util.UtilAuth;
+import com.project.miinhareceita.tests.UserFactory;
+import com.project.miinhareceita.user.domain.User;
+import com.project.miinhareceita.user.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+@ExtendWith(SpringExtension.class)
+public class AuthServiceTest {
+
+    @InjectMocks
+    private AuthService authService;
+
+    @Mock
+    private UtilAuth utilAuth;
+
+    @Mock
+    private UserRepository userRepository;
+
+    private String correctUserNameToken, invalidUserNameToken, existingUserName, nonExistingUsername;
+    private User user;
+
+
+
+    @BeforeEach
+    void setUp(){
+        user = UserFactory.createUser();
+        existingUserName = "kkowalskigustavo@gmail.com";
+        nonExistingUsername =  "maria@gmail.com";
+
+
+        Mockito.when(userRepository.findByEmail(existingUserName)).thenReturn(Optional.of(user));
+        Mockito.when(userRepository.findByEmail(nonExistingUsername)).thenReturn(Optional.empty());
+    }
+
+    @Test
+    public void authenticatedShouldReturnUserWhenSucess(){
+        Mockito.when(utilAuth.getLoggedToken()).thenReturn(existingUserName);
+
+        User result = authService.authenticated();
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(result.getUsername(), existingUserName);
+    }
+    @Test
+    public void authenticatedShouldReturnUserNameNotFoundExceptionWhenUserDoesNotExist(){
+        Mockito.when(utilAuth.getLoggedToken()).thenThrow(ClassCastException.class);
+
+        Assertions.assertThrows(UsernameNotFoundException.class, () -> {
+            authService.authenticated();
+        });
+    }
+}
+
