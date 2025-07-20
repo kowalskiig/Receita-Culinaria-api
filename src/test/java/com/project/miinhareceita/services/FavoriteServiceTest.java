d6e018ebbc895576abd63a9bc9c540936b643f51
nawszin <kkowalskigustavo@gmail.com>
2025-07-20 19:30:21 -0300

    feat(Favorite): teste unitario testes dos métodos de validação e getFavoriteRecipesMe implementados

--- src/test/java/com/project/miinhareceita/services/FavoriteServiceTest.java
+++ src/test/java/com/project/miinhareceita/services/FavoriteServiceTest.java
@@ -7,6 +7,7 @@ import com.project.miinhareceita.favorite.repository.FavoriteRepository;
 import com.project.miinhareceita.favorite.service.FavoriteService;
 import com.project.miinhareceita.recipe.domain.Recipe;
 import com.project.miinhareceita.recipe.repository.RecipeRepository;
+import com.project.miinhareceita.shared.exceptions.ConflictException;
 import com.project.miinhareceita.shared.exceptions.ResourceNotFoundException;
 import com.project.miinhareceita.tests.FavoriteFactory;
 import com.project.miinhareceita.tests.RecipeFactory;
@@ -21,6 +22,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -45,37 +48,45 @@ public class FavoriteServiceTest {
     private Recipe recipe;
     private User user;
     private Favorite favorite;
+    private List<Favorite> favoriteList;
 
     @BeforeEach
     void setUp(){
         existingRecipeId = 1L;
         nonExistingRecipeId = 2L;
 
+
+
         existingUserId =1L;
         nonExistingUserId =2L;
 
         recipe = RecipeFactory.createRecipe();
         user = UserFactory.createUser();
-
         favorite = FavoriteFactory.createFavorite(recipe, user);
+        favoriteList = new ArrayList<>();
+        favoriteList.add(favorite);
+
 
         Mockito.when(authService.authenticated()).thenReturn(user);
 
         Mockito.when(recipeRepository.findById(existingRecipeId)).thenReturn(Optional.of(recipe));
-        Mockito.when(recipeRepository.findById(nonExistingRecipeId)).thenThrow(ResourceNotFoundException.class);
+        Mockito.when(recipeRepository.findById(nonExistingRecipeId)).thenReturn(Optional.empty());
 
-        Mockito.when(favoriteRepository.existsByUserIdAndRecipeId(existingUserId, existingRecipeId))
-                .thenReturn(false);
+
 
         Mockito.when(recipeRepository.getReferenceById(existingRecipeId)).thenReturn(recipe);
 
         Mockito.when(favoriteRepository.save(any())).thenReturn(favorite);
 
+        Mockito.when(favoriteRepository.existsByUserIdAndRecipeId(existingUserId, existingRecipeId))
+                .thenReturn(false);
 
+        Mockito.when(favoriteRepository.findFavoritesByUserId(existingUserId)).thenReturn(favoriteList);
 
     }
     @Test
     public void insertFavoriteRecipeShouldReturnFavoriteDtoWhenSucess(){
+
         FavoriteDTO result = favoriteService.insertFavoriteRecipe(existingRecipeId);
         Assertions.assertNotNull(result);
         Assertions.assertEquals(result.getRecipeFavoriteDTO().getId(), recipe.getId());
@@ -86,6 +97,34 @@ public class FavoriteServiceTest {
         Mockito.verify(favoriteRepository, Mockito.times(1)).existsByUserIdAndRecipeId(existingUserId, existingRecipeId);
         Mockito.verify(favoriteRepository, Mockito.times(1)).save(any());
         Mockito.verify(authService, Mockito.times(1)).authenticated();
+    }
+    @Test
+    public void insertFavoriteRecipeShourldReturnResourceNotFoundExceptionWhenRecipeIdDoesNotExist(){
+
+            Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+
+            favoriteService.insertFavoriteRecipe(nonExistingRecipeId);
+
+        });
+    }
+    @Test
+    public void insertFavoriteRecipeShourldReturnResourceNotFoundExceptionWhenRecipeIdDoesNotResponseToUserAuthenticated(){
+
+        Mockito.when(favoriteRepository.existsByUserIdAndRecipeId(existingUserId, existingRecipeId))
+                .thenReturn(true);
+        Assertions.assertThrows(ConflictException.class, () -> {
+
+            favoriteService.insertFavoriteRecipe(existingRecipeId);
+
+        });
+    }
+
+    @Test
+    public void getFavoritesRecipeMeShouldReturnListFavoriteDtoWhenSucess(){
+        List<FavoriteDTO> result = favoriteService.getFavoriteRecipesMe();
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(result.size(), favoriteList.size());
+
     }
 
 }
