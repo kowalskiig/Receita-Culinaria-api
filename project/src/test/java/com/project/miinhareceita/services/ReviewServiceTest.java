56bfe8306be751370df71f9b768adb8c5586d599
Gustavo <kkowakskigustavo@gmail.com>
2025-08-20 01:15:07 -0300

    test(ReviewServiceTest): create ReviewServiceTest

---
+++ project/src/test/java/com/project/miinhareceita/services/ReviewServiceTest.java
@@ -0,0 +1,11 @@
+package com.project.miinhareceita.services;
+
+import com.project.miinhareceita.review.service.ReviewService;
+import org.mockito.InjectMocks;
+
+public class ReviewServiceTest {
+    @InjectMocks
+    private ReviewService reviewService;
+
+    
+}
